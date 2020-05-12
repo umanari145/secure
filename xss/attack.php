60fba1e608fcec318bb8242d145f27fd6b355f2e
Norio <umanari145@gmail.com>
2020-05-12 21:43:43 +0900

    XSSの修正とREADMEの修正

--- xss/attack.php
+++ xss/attack.php
@@ -3,6 +3,6 @@
   $sid = $_GET['sid'];
   //ここの下に何らかの攻撃処理をかく(たとえば攻撃した人間に被害者のSESSIONIDを記したメールを送るなど)
 ?>
-<body>攻撃成功<br>
+<body>攻撃成功(sampleからこの画面に強引に遷移させる)<br>
 <?php echo $sid;?>
 </body>
--- xss/simple.php
+++ xss/simple.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
-//下記URLでアクセスするとSESSIONIDが見える
-//http://localhost/secure/xss/simple.php?keyword=<script>alert(document.cookie)</script>
 ?>
 <body>
+<!--echo $_GET["keyword"]のなかにJavaScriptを埋め込まれてしまう--> 
 検索キーワード:<?php echo $_GET["keyword"]; ?>
 </body>
